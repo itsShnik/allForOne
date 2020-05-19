bug fix: softmax, treating multiple grus as module list

openvqa/models/allForOne/attentionTeams.py
@@ -8,13 +8,6 @@ import torch.nn.functional as F
 import torch
 import math
 
-# -------------------
-# ---- GRU units ----
-# -------------------
-
-def gru_unit(__C):
-    return nn.GRU(input_size=__C.WORD_EMBED_SIZE, hidden_size=__C.HIDDEN_SIZE, num_layers=1, batch_first=True).cuda()
-
 # ----------------------------
 # ---- Additive Attention ----
 # ----------------------------
@@ -25,7 +18,7 @@ class Attention(nn.Module):
 
         # initialize a linear layer to project to a single dimension
         self.linear = nn.Linear(2*__C.HIDDEN_SIZE, 1)
-        self.softmax = nn.Softmax(-1)
+        self.softmax = nn.Softmax(1)
 
     def forward(self, feats):
         attention_weights = self.linear(feats)
@@ -43,10 +36,20 @@ class AttentionTeams(nn.Module):
         super(AttentionTeams, self).__init__()
 
         # initialize num_teams number of GRU units
-        self.unit_list = [gru_unit(__C) for _ in range(__C.NUM_TEAMS)]
+        self.unit_list = nn.ModuleList([nn.GRU(
+                input_size=__C.WORD_EMBED_SIZE,
+                hidden_size=__C.HIDDEN_SIZE,
+                num_layers=1, 
+                batch_first=True
+            ) for _ in range(__C.NUM_TEAMS)])
 
         # initialize a lead unit
-        self.lead_unit = gru_unit(__C)
+        self.lead_unit = nn.GRU(
+                input_size=__C.WORD_EMBED_SIZE,
+                hidden_size=__C.HIDDEN_SIZE,
+                num_layers=1, 
+                batch_first=True
+            )
 
         # initialize a self-attention module
         self.attention = Attention(__C)
@@ -54,12 +57,13 @@ class AttentionTeams(nn.Module):
     def forward(self, lang_feat, img_feat):
 
         # concatenate the input along objects and words
-        feat = torch.cat((lang_feat, img_feat), -2)
+        feat = torch.cat((img_feat,lang_feat), -2)
 
         # create a matrix of vectors obtained using each unit
         unit_feat_list = []
 
         for i in range(len(self.unit_list)):
+            self.unit_list[i].flatten_parameters()
             _, temp_vec = self.unit_list[i](feat)
             temp_vec = temp_vec.transpose(0,1)
             unit_feat_list.append(temp_vec)
@@ -68,6 +72,7 @@ class AttentionTeams(nn.Module):
         unit_feats = torch.cat(unit_feat_list, -2)
 
         # lead unit features
+        self.lead_unit.flatten_parameters()
         _, lead_unit_feat = self.lead_unit(feat) 
         lead_unit_feat = lead_unit_feat.transpose(0,1)
 